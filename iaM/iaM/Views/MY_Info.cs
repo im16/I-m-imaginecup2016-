31a2782806b58aa36ea3734338cc5a7141bfcccb
CrazyBogus <crazyybogus@gmail.com>
2016-02-13 18:12:14 +0900

    0213
    Edit Complete

---
+++ iaM/iaM/Views/MY_Info.cs
@@ -0,0 +1,12 @@
+﻿namespace iaM.Views
+{
+    class My_Info
+    {
+        public string id { get; set; }
+        public string nickname { get; set; }
+        public string phoneNum { get; set; }
+        public string status_message { get; set; }
+        public string other_sns { get; set; }
+
+    }
+}
